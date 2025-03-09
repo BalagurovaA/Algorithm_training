eab41eae244ff3c4321bc0256821b1edff120492
BalagurovaA <balagurova0473@mail.ru>
2025-03-09 16:45:26 +0300

    обновила задачку про римские цифры

--- swift/leetcode/leetcode_roman_to_Integer/leetcode_roman_to_Integer/main.swift
+++ swift/leetcode/leetcode_roman_to_Integer/leetcode_roman_to_Integer/main.swift
@@ -14,25 +14,18 @@ class Solution {
 
         var numberType: [Character: Int] = ["I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000]
         
-        var str = String(s.reversed())
-        
         var total = 0
-        for i in 0..<str.count {
-//        for i in (0..<s.count).reversed() {
- 
-            if i == 0 {
-                total += numberType[s[i]] ?? 0
-            }
-            else if numberType[s[i]]!  < numberType[s[i - 1]]! {
-                total -= numberType[s[i]]!
+        
+        for i in 0..<s.count {
+            var curNumb = numberType[s[i]]!
+            var nextNumb = numberType[s[i + 1]]!
+            if s[i] < s[i + 1] {
+                total -= curNumb
             } else {
-                total += numberType[s[i]]!
+                total += curNumb
             }
         }
-        
-        
-        
-        
+
         return total
     }
 }
