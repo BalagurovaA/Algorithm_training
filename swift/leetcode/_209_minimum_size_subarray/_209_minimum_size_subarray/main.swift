2f3c2cbedd5f02e496a04ab5f391a25c1deae784
BalagurovaA <balagurova0473@mail.ru>
2025-03-16 22:09:33 +0300

    доделать задачу 209

--- swift/leetcode/_209_minimum_size_subarray/_209_minimum_size_subarray/main.swift
+++ swift/leetcode/_209_minimum_size_subarray/_209_minimum_size_subarray/main.swift
@@ -1,11 +1,42 @@
-//
-//  main.swift
-//  _209_minimum_size_subarray
-//
-//  Created by Kristofer Sartorial on 3/16/25.
-//
 
 import Foundation
 
-print("Hello, World!")
+class Solution {
+    func minSubArrayLen(_ target: Int, _ nums: [Int]) -> Int {
+        
+        for i in 0..<nums.count {
+            if nums[i] == target {
+                return 1
+            }
+        }
+        
+        var right = 0
+        var left = 0
+        var sum = 0
+        var counter = 0
+        var minCounter = 0
+        
+        for i in 0..<nums.count {
 
+            sum += nums[i]
+            
+            counter += 1
+            if sum >= target {
+                sum -= nums[left]
+                left += 1
+                counter -= 1
+            } else {
+                
+                right = i + 1
+            }
+            
+        }
+        
+        return counter
+    }
+}
+
+var s = Solution()
+var a = [2,3,1,2,4,3]
+
+print(s.minSubArrayLen(7, a))
