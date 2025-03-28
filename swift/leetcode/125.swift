07e3ae97173e9566ae760c8d39ffc4c2a8671dfb
BalagurovaA <balagurova0473@mail.ru>
2025-03-28 20:48:32 +0300

    leetcode 3

---
+++ swift/leetcode/125.swift
@@ -0,0 +1,31 @@
+// 125
+import Foundation
+
+class Solution {
+    func isPalindrome(_ s: String) -> Bool {
+        
+        var str = s.lowercased().filter {$0.isLetter || $0.isNumber}
+        
+        if str.isEmpty {return true}
+        
+        var left = str.startIndex
+        var right = str.index(str.endIndex, offsetBy: -1)
+        
+        while left < right {
+            if str[left] != str[right]{
+                return false
+                
+            } else {
+                left = str.index(after: left)
+                right = str.index(before: right)
+            }
+        }
+        return true
+    }
+}
+
+var s = Solution()
+var arr = "0P"
+print(s.isPalindrome(arr))
+
+
--- swift/leetcode/14.swift
+++ swift/leetcode/14.swift
@@ -1,23 +1,27 @@
 // 14
+
 class Solution {
     func longestCommonPrefix(_ strs: [String]) -> String {
-        
-        var firstStr = Array(strs[0])
+        if strs.isEmpty { return "" }
+        if strs.contains("") { return "" }
         var res = ""
 
-        for i in 0..<firstStr.count {
+        let firstStrArr = Array(strs[0])
+
+        for i in 0..<firstStrArr.count {
             for s in strs {
-                if i >= s.count || firstStr[i] != Array(s)[i] {
+                if i >= Array(s).count || firstStrArr[i] != Array(s)[i]  {
                     return res
                 }
             }
-            res += String(firstStr[i])
+            res += String(Array(strs[0])[i])
         }
-        
         return res
     }
 }
+
 var sol = Solution()
 var s1 = ["tag", "flag", "mag"]
 
 print(sol.longestCommonPrefix(s1))
+
