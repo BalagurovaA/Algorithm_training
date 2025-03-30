29ce626a5c9c1b6aa43a8b9980334418591a39cd
BalagurovaA <balagurova0473@mail.ru>
2025-03-30 19:08:08 +0300

    ffff

--- tink/tink_3.swift
+++ tink/tink_3.swift
@@ -1,8 +1,35 @@
-//
-//  tink_3.swift
-//  
-//
-//  Created by Kristofer Sartorial on 3/29/25.
-//
-
 import Foundation
+
+func planetAttack() ->Int {
+    
+    var months = Int(readLine()!)!
+    let attack = Array(readLine()!)
+    
+    if attack.isEmpty {return 0}
+    
+    var popAttcack: [String: Int] = ["A": 0, "B": 0, "C": 0]
+    var unpleasent: [String: Int] = ["A": 0, "B": 0, "C": 0]
+    
+    for i in 0..<attack.count {
+        let type = String(attack[i])
+        popAttcack[type]! += 1
+        
+        let maxQuantity = popAttcack.values.max()
+        
+        for (type, count) in popAttcack {
+            if popAttcack[type]! == maxQuantity {
+                unpleasent[type]! += 1
+            }
+            
+        }
+        
+    }
+    let maxUnpleasent = unpleasent.values.max()
+    return Int(maxUnpleasent!)
+}
+
+
+
+print(planetAttack())
+
+
